e66e1c22fc6165c257f021cbf7195e1c4b5b32bf
Francesco Santini <francescotn02@gmail.com>
2023-03-17 11:10:58 +0000

    better santini.py

--- santini.py
+++ santini.py
@@ -6,15 +6,15 @@ from Crypto.Protocol.KDF import scrypt
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
 
-
 # custom errors
 class SymEncError(Exception):
     '''Error executing Symmetric Encryption script'''
 
-
 def getKeyByPsw(psw: str, salt):
     '''
         This method generate a key from a password
+        It uses a Key Derivation Function named 'scrypt'
+
         Parameters:
         - psw: password. IT MUST BE A STRING
         - salt: if not specified or the length is incorrect it will generate a new 16 byte salt
@@ -28,13 +28,12 @@ def getKeyByPsw(psw: str, salt):
     key = scrypt(psw, salt, 16, N=2**20, r=8, p=1)
     return key, salt
 
-
 def save_and_exit(path, password, credentials):
     data = json.dumps(credentials, ensure_ascii=False).encode('utf-8')
     
-    # key, salt = getKeyByPsw(password, '')
     salt = get_random_bytes(16)
     key = scrypt(password, salt, 16, N=2**20, r=8, p=1)
+    # AES is used (standard) for symmetric encryption
     cipher = AES.new(key, AES.MODE_OCB)
     ciphertext, tag = cipher.encrypt_and_digest(data)
     
@@ -63,7 +62,6 @@ def search_and_add(query, dic):
 
 def load_data(path, password):
     with open(path, 'rb') as in_file:
-        # key, _ = getKeyByPsw(password, in_file.read(16))
         key = scrypt(password, in_file.read(16), 16, N=2**20, r=8, p=1)
         nonce = in_file.read(15)
         tag = in_file.read(16)
@@ -83,6 +81,7 @@ def load_data(path, password):
     return credentials
 
 def log_in(username, password):
+    # the script uses BLAKE2 because it is very fast on modern hardware.
     blake_hash = BLAKE2b.new(data = username.encode('utf-8'), digest_bytes=64)
     path_file = blake_hash.hexdigest()
     
@@ -120,28 +119,16 @@ def log_in(username, password):
             except IOError:
                 print('Error while saving, new data has not been updated!')
             return
-    
 
-
-
-
-def main():
-    while True:
-        print('Insert username and password to load data,')
-        print('leave blank and press "enter" to exit.')
-        username = input('Username: ')
-        if username == '':
-            print('Goodbye!')
-            exit()
-        else:
-            # leggi la password in maniera opportuna
-            password = getpass('Password: ')
-            log_in(username, password)
-
-'''
-    start
-    Python Best Practice
-    Checks what's running the script and, if it's just imported functions, it doesn't run the main method.
-'''
-if __name__ == '__main__':
-    main()
+#* MAIN
+while True:
+    print('Insert username and password to load data,')
+    print('leave blank and press "enter" to exit.')
+    username = input('Username: ')
+    if username == '':
+        print('Goodbye!')
+        exit()
+    else:
+        # read password properly
+        password = getpass('Password: ')
+        log_in(username, password)
